07371ee17ff307a1dfc10f35aaecbf637ef14ada
Ojārs <ojars.meldris@gmail.com>
2022-11-23 22:16:36 +0200

    update

--- index.php
+++ index.php
@@ -4,12 +4,10 @@ require_once 'vendor/autoload.php';
 
 use App\ApiClient;
 
-const API_KEY='cf60e647aafa33c963a3f5a489d1964b';
-
-$selectedLocation=$_GET['city'] ?? 'Riga';
+const API_KEY='';//ad your key!
 
 $apiClient=new ApiClient(API_KEY);
-$weather = $apiClient->getWeather($selectedLocation);
+$weather = $apiClient->getWeather($_GET['city'] ?? 'Riga');
 ?>
 
 <!doctype html>
@@ -20,24 +18,35 @@ $weather = $apiClient->getWeather($selectedLocation);
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>weather</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <a href="/?city=Riga">Riga</a>
-    <a href="/?city=Tallinn">Tallinn</a>
-    <a href="/?city=Vilnius">Vilnius</a>
-    <h1>
-        Weather report in <?php echo $weather->getLocationName() ?>
-    </h1>
-    <p>
+<h1 class="title">
+    Weather report in <?php echo $weather->getLocationName() ?>
+</h1>
+<div class="box-container">
+    <div class="box box1 text">
+        city:<br>
+        <a href="/?city=Riga">Riga</a><br>
+        <a href="/?city=Tallinn">Tallinn</a><br>
+        <a href="/?city=Vilnius">Vilnius</a><br>
+    </div>
+    <div class="box box2 text">
         temperature is <?php echo $weather->getTemperature() ?> °C <br>
         feels like <?php echo $weather->getTemperatureFeelsLike() ?> °C <br>
         humidity is <?php echo $weather->getHumidity() ?> % <br>
         wind speed is <?php echo $weather->getWindSpeed() ?> m/s <br>
-        <img src="<?php echo $weather->getWeatherIcon() ?>">
-    </p>
-    <form>
-        <label>Search </label>
-        <input type="text" id="city" name="city" placeholder="city name">
-    </form>
+    </div>
+    <div class="box box3 text">
+        <img src="<?php echo $weather->getWeatherIcon() ?>" alt="image" width="150">
+    </div>
+    <div class="box box4">
+        <form>
+            <label for="city">Search:</label>
+            <input style="font-size: 25px" type="text" id="city" name="city" placeholder="city name">
+        </form>
+    </div>
+</div>
 </body>
 </html>
+
